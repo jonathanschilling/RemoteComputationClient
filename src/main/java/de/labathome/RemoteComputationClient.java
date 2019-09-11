e797d3a71cff77a5800e81e82a2a178a14a40545
Jonathan Schilling <jonathan.schilling@ipp.mpg.de>
2019-09-11 16:30:38 +0200

    basic example working

--- src/main/java/de/labathome/RemoteComputationClient.java
+++ src/main/java/de/labathome/RemoteComputationClient.java
@@ -4,6 +4,7 @@ import de.labathome.RemoteComputationKernelGrpc.RemoteComputationKernelBlockingS
 import de.labathome.RemoteComputationKernelGrpc.RemoteComputationKernelStub;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
 
 public class RemoteComputationClient {
 
@@ -22,6 +23,96 @@ public class RemoteComputationClient {
 		channel = channelBuilder.build();
 		blockingStub = RemoteComputationKernelGrpc.newBlockingStub(channel);
 		asyncStub = RemoteComputationKernelGrpc.newStub(channel);
+	}
+
+	public void reset() {
+
+		ResetRequest req = ResetRequest.newBuilder().build();
+		ResetResult result;
+		try {
+			result = blockingStub.reset(req);
+		} catch (StatusRuntimeException e) {
+			System.out.println("RPC failed: "+e.getStatus());
+			e.printStackTrace();
+		}
+
+	}
+
+	public void sendSomeData() {
+
+		SendVariableRequest req = SendVariableRequest
+				.newBuilder()
+				.setVariableName("testVariable")
+				.addData(42.0)
+				.addData(41.0)
+				.build();
+		SendVariableResult result;
+		try {
+			result = blockingStub.sendVariable(req);
+		} catch (StatusRuntimeException e) {
+			System.out.println("RPC failed: "+e.getStatus());
+			e.printStackTrace();
+		}
+	}
+
+	public void execute() {
+		
+		ExecuteRequest req = ExecuteRequest.newBuilder().build();
+		ExecuteResult res;
+		try {
+			res = blockingStub.execute(req);
+		} catch (StatusRuntimeException e) {
+			System.out.println("RPC failed: "+e.getStatus());
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void queryData() {
+
+		String varName = "testVariable";
+
+		GetVariableRequest req = GetVariableRequest
+				.newBuilder()
+				.setVariableName(varName)
+				.build();
+		GetVariableResult res;
+		try {
+			res = blockingStub.getVariable(req);
+
+			System.out.println("got data for variable "+varName);
+			if (res.getStatus()==0) {
+				for (int i=0; i<res.getDataCount(); ++i) {
+					System.out.println(varName+"["+i+"]="+res.getData(i));
+				}
+			} else {
+				System.out.println(varName+" not found");
+			}
+		} catch (StatusRuntimeException e) {
+			System.out.println("RPC failed: "+e.getStatus());
+			e.printStackTrace();
+		}
+
+	}
+
+	
+	
+
+	public static void main(String[] args) {
+
+		// connect
+		RemoteComputationClient client = new RemoteComputationClient("localhost", 50051);
+
+
+		// clean environment
+		client.reset();
+
+		client.sendSomeData();
+
+		client.execute();
+		
+		client.queryData();
+
 	}
 
 }
